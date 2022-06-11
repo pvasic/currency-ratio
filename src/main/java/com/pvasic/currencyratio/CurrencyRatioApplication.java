fa8ab7428ea58a626f12f8ef3e2e9a3f335cbaf3
Pavel Vasichkin <pl.vasichkin@gmail.com>
2022-06-11 13:33:24 +0300

    Initial project

---
+++ src/main/java/com/pvasic/currencyratio/CurrencyRatioApplication.java
@@ -0,0 +1,13 @@
+package com.pvasic.currencyratio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRatioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyRatioApplication.class, args);
+    }
+
+}
