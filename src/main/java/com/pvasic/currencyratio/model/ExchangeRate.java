30895b559cae75e36d0a8f597b1b5e30527a82af
Pavel Vasichkin <pl.vasichkin@gmail.com>
2022-06-15 15:33:24 +0300

    add FeignRatesClientTest

--- src/main/java/com/pvasic/currencyratio/model/ExchangeRate.java
+++ src/main/java/com/pvasic/currencyratio/model/ExchangeRate.java
@@ -1,5 +1,6 @@
 package com.pvasic.currencyratio.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import java.util.Map;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ExchangeRate {
     private Map<String, Double> rates;
 }
